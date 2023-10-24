153999a4e4273269a8b370fbd065c60436fd46fe
Garito <garito@gmail.com>
2023-10-24 18:35:30 +0200

    join + homepage mobile menu

---
+++ assets/basic.js
@@ -0,0 +1,66 @@
+const join = async email => {
+  const url = 'https://ipfs.lateralthink.club/mailchimp/rackz/join';
+  const opts = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email_address: email })
+  }
+  const resp = await fetch(url, opts);
+  if (resp.ok) {
+    confetti({
+      spread: 360,
+      ticks: 200,
+      gravity: 1,
+      decay: 0.94,
+      startVelocity: 30,
+      particleCount: 100,
+      scalar: 3,
+      shapes: ["image"],
+      shapeOptions: {
+        image: [
+          {
+            src: "./logo.svg",
+            width: 32,
+            height: 32,
+          }
+        ]
+      }
+    });
+  } else {
+    const err = await resp.json();
+    throw new Error('Error joining', { cause: err.detail });
+  }
+}
+
+const joinDesktop = async e => {
+  e.preventDefault();
+  try {
+    await join(e.target.email.value);
+    e.target.email.value = '';
+  } catch (err) { console.log(err.cause) }
+}
+
+const joinMobile = async e => {
+  e.preventDefault();
+  try {
+    await join(e.target['mobile-email'].value);
+    e.target['mobile-email'].value = '';
+  } catch (err) { console.log(err.cause) }
+}
+
+window.addEventListener('load', () => {
+  const burger = document.getElementById('burger');
+  burger.addEventListener('click', () => {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.remove('hidden');
+    const close = document.getElementById('close-menu');
+    close.addEventListener('click', () => {
+      menu.classList.add('hidden');
+    });
+  });
+
+  const joinForm = document.getElementById('join');
+  joinForm.addEventListener('submit', joinDesktop);
+  const mobileJoinForm = document.getElementById('mobile-join');
+  mobileJoinForm.addEventListener('submit', joinMobile);
+});
